4773e89fcf767841abbd7cc81939fe928032e912
Pedro F <pedrospf2018@gmail.com>
2025-07-07 08:09:21 -0300

    feat: implement Grade Curricular page

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
   return (
     <div className="container mx-auto px-4 py-8">
       <HomeHeader />
-
+      
       <CoursesInProgressSection />
 
       <AllAvailableCoursesSection />
