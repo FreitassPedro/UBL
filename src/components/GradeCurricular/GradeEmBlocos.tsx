c6589e353fba0ac4aa9ad7ec8f6060f699bc351e
Pedro F <pedrospf2018@gmail.com>
2025-07-10 07:20:51 -0300

    refactor: move components

---
+++ src/components/GradeCurricular/GradeEmBlocos.tsx
@@ -0,0 +1,31 @@
+import { curriculum } from "../../data/curriculum";
+import { SubjectItem } from "./SubjectItem";
+
+export function GradeEmBlocos() {
+    const semesters = curriculum;
+
+    return (
+        <section className="mb-16">
+            {/* Título com texto branco */}
+            <h2 className="text-3xl text-white font-bold">Currículo em etapas</h2>
+            <div className="space-y-6 ">
+                {semesters.map((semester) => (
+                    // Fundo do bloco do semestre ajustado para cinza escuro, sombra
+                    <div key={semester.number} className="bg-gray-800/20 shadow-xl rounded-xl p-6">
+                        {/* Título do semestre com texto branco */}
+                        <h4 className="text-xl font-semibold text-white mb-4">Semestre {semester.number}</h4>
+                        <span className="font-medium text-white">Disciplina / </span>
+                        <span className="text-gray-300">Pré-requisitos</span>
+                        <ul className="mt-4 space-y-4">
+                            {semester.subjects.map((subject) => (
+                                <SubjectItem key={subject.name} subject={subject} />
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
+
+}
+
