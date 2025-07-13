9f389aad00945aa73ffadadafd5a571594a91a5d
Pedro F <pedrospf2018@gmail.com>
2025-07-13 17:59:22 -0300

    feat: Estrutura Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,9 @@
 import { Link } from "react-router-dom";
 
-
 export const Navbar = () => {
     return (
-        <div className="sticky top-0 left-0 z-50 bg-gray-800 shadow-xl border-b border-gray-700 ">
-            <div className="flex justify-between mx-w-7xl mx-auto px-6 py-4 items-center">
+        <header className="sticky top-0 left-0 z-50 bg-gray-800 shadow-xl border-b border-gray-700 ">
+            <div className="flex justify-between max-w-7xl mx-auto px-6 py-4 items-center">
                 <div className="flex flex-col">
                     <Link to={"/"} className="flex items-center space-x-2">
                         <h1 className="text-4xl font-bold bg-gradient-to-r from-blue-600 to-cyan-400 bg-clip-text text-transparent">
@@ -17,11 +16,11 @@ export const Navbar = () => {
                     <Link to="/" className="text-gray-300 hover:font-medium hover:text-white">Home</Link>
                     <Link to="/grade-curricular" className="text-gray-300 hover:font-medium hover:text-white"> Grade Curricular</Link>
                     <Link to="/meu-curso" className="text-gray-300 hover:font-medium hover:text-white">Meu Curso</Link>
-                    <Link to="https://github.com/Universidade-Livre" className="text-gray-300 hover:font-medium hover:text-white">Github</Link>
+                    <Link to="https://github.com/Universidade-Livre" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:font-medium hover:text-white">Github</Link> {/* Adicionado target/rel para link externo */}
                 </div>
 
 
             </div>
-        </div>
+        </header>
     )
 };
