92e38fd9cecffc6f262a38e7296ef4b661ac9762
Pedro F <pedrospf2018@gmail.com>
2025-12-04 13:02:30 -0300

    feat: Scroll Videos

--- src/components/CourseContent/VideoPlayer.tsx
+++ src/components/CourseContent/VideoPlayer.tsx
@@ -1,5 +1,3 @@
-import { PlayIcon } from "lucide-react";
-
 export function VideoPlayer({ videoId }: { videoId: string }) {
   return (
     <div className="w-full h-[400px] my-4 bg-bg-card rounded-xl shadow-2xl shadow-black/40 overflow-hidden border border-border-subtle">
--- src/pages/CoursePage/CoursePage.tsx
+++ src/pages/CoursePage/CoursePage.tsx
@@ -4,7 +4,6 @@ import { Link, useParams } from "react-router-dom";
 import { useCourseProgress } from "../../contexts/CourseProgressContext";
 
 import { VideoPlayer } from "../../components/CourseContent/VideoPlayer";
-import { CourseSidebar } from "../../components/CourseContent/CourseSidebar";
 import { CurriculoCC } from "../../data/gradeCurricular";
 import type { MyCadeiraProgress, MyLesson } from "../../data/myCourseProgress";
 import { mapCadeiraToMyCadeira } from "../../lib/utils";
@@ -13,9 +12,8 @@ import useTituloDaPagina from "../../contexts/useTitlePage";
 
 export default function CoursePage() {
   useTituloDaPagina('Curso');
-
   const { id } = useParams<{ id: string }>();
-  const { completedLessons } = useCourseProgress();
+  const { toggleCompletion, completedLessons } = useCourseProgress();
 
   const myCadeira: MyCadeiraProgress | undefined = useMemo(() => {
     if (!id) return;
@@ -31,6 +29,40 @@ export default function CoursePage() {
     return myCadeira.lessons[0];
   });
 
+
+  const ClassSideBarItem: React.FC<{ lesson: MyLesson; isSelected: boolean; onSelect: (lesson: MyLesson) => void }> = ({ lesson }) => {
+
+    let statusClass = '';
+    let statusContent = '';
+
+    if (lesson.isCompleted) {
+      statusClass = 'bg-success border-success';
+      statusContent = '✓';
+    } else {
+      statusClass = 'border-border-subtle';
+    }
+
+
+    return (
+      <li className="flex items-center cursor-pointer py-2 border-b border-white/10 "
+        key={lesson.id}
+      >
+        <div>
+          <button className={`w-4 h-4 flex items-center justify-center rounded-full border ${statusClass} mr-3`}
+            onClick={() => toggleCompletion(lesson.id)}
+          >
+            <span className="text-xs text-black font-bold">{statusContent}</span>
+          </button>
+        </div>
+        <div onClick={() => handleSelectLesson(lesson)}
+        >
+          <span className={`${lesson.id === selectedLesson?.id ? 'text-text-main font-medium' : 'font-light text-text-muted'} hover:text-gray-200 text-sm px-2`}>{lesson.title}</span>
+          <span className="text-xs text-zinc-800">{lesson.duration}</span>
+        </div>
+      </li>
+    )
+
+  }
   const handleSelectLesson = (lesson: MyLesson) => {
     setSelectedLesson(lesson);
   };
@@ -53,7 +85,7 @@ export default function CoursePage() {
 
   return (
     <div className="min-h-screen bg-bg-body text-text-main flex justify-center overflow-hidden font-inter p-6">
-      <div className="w-full max-w-6xl h-[85vh] grid grid-cols-1 lg:grid-cols-[2.5fr_1fr] gap-6">
+      <div className="w-full my-6 max-w-6xl h-[85vh] grid grid-cols-1 lg:grid-cols-[2.5fr_1fr] gap-6">
         <main className="flex flex-col p-8 bg-bg-card border border-zinc-800 rounded-xl shadow-2xl shadow-black/40 overflow-hidden">
           { /* video player */}
           <span className="text-sm font-medium text-gray-300">{`Aula > ${myCadeira.id} > ${myCadeira.name}`}</span>
@@ -61,7 +93,8 @@ export default function CoursePage() {
             <VideoPlayer videoId={selectedLesson!.videoId} />
           </div>
           { /* professor infos */}
-          <div className="flex items-center gap-4 mb-4">
+          <div className="flex items-center gap-4 mb-4"
+            onClick={() => toggleCompletion(selectedLesson?.id)}>
             <input type="checkbox" />
             <span>Aula Concluída</span>
           </div>
@@ -70,21 +103,16 @@ export default function CoursePage() {
         </main>
 
         { /* sidebar com lista de aulas */}
-        <aside className="flex flex-col gap-6 p-8 bg-bg-card border border-zinc-800 rounded-xl">          {/* Videos */}
+        <aside className="flex flex-col gap-6 p-8 overflow-hidden bg-bg-card border border-zinc-800 rounded-xl">
+          <h3>Playlist de Aulas</h3>
+
+          {/* Videos */}
+
           <div className="flex flex-col h-full overflow-hidden">
-            <h3>Playlist de Aulas</h3>
+            <span className="text-sm text-text-muted">{`999 de ${myCadeira.lessons.length}`}</span>
             <ul className="overflow-y-auto pr-2">
               {myCadeira.lessons.map((lesson) => (
-                <li className="flex items-center cursor-pointer py-2 border-b border-white/10 "
-                  key={lesson.id}
-                  onClick={() => handleSelectLesson(lesson)}
-                >
-                  <div>
-                    {lesson.id === selectedLesson?.id ? '▶️' : '•'}
-                  </div>
-                  <span className={`${lesson.id === selectedLesson?.id ? 'text-text-main font-medium' : 'font-light text-text-muted'} hover:text-gray-200 text-sm px-2`}>{lesson.title}</span>
-                  <span className="text-xs text-zinc-800">{lesson.duration}</span>
-                </li>
+                <ClassSideBarItem key={lesson.id} lesson={lesson} isSelected={selectedLesson?.id === lesson.id} onSelect={handleSelectLesson} />
               ))}
             </ul>
           </div>
