ab0820b86772e1db965f5934352c937dd51ca95e
Pedro F <pedrospf2018@gmail.com>
2025-12-19 20:58:21 -0300

    feat: UBL colors and updated Progress Component

--- src/components/ui/TesteProgress.tsx
+++ src/components/ui/TesteProgress.tsx
@@ -1,22 +1,23 @@
-import * as ProgressPrimitive from "@radix-ui/react-progress"; 
+import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 interface TesteProgressProps {
   value: number; // Valor do progresso (0-100)
-  color?: string;
+  color?: string; // Opcional
+  height?: string; // Altura opcional da barra de progresso
 }
 
 // Componente antigo de progresso NextJS não funciona bem
-export function TesteProgress({ value, color }: TesteProgressProps) {
-  // O essencial ter algum `background-color` para ver o preenchimento.
+export function TesteProgress({ value, color, height }: TesteProgressProps) {
 
   return (
-    <div className="h-2 w-full bg-gray-300 rounded-full overflow-hidden"> {/* Contêiner da barra */}
+    <div className={`w-full bg-gray-300 rounded-full overflow-hidden ${height ? height : 'h-2'}`}>
       <ProgressPrimitive.Root
         className="relative h-full w-full"
         value={value}
       >
+
         <ProgressPrimitive.Indicator
-          className={`h-full ${color ? color : 'bg-blue-400'}  transition-transform duration-500 ease-in-out`} // Estilo da barra de preenchimento
+          className={`h-full ${color ? color : 'from-ubl-blue to-ubl-green bg-linear-to-r'} transition-transform duration-500 ease-in-out`} // Estilo da barra de preenchimento
           style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
         />
       </ProgressPrimitive.Root>
