954feadd6e1395fd57d6a0cc4fda067798d46cb1
nayetdet <jpmoreiracampos21@gmail.com>
2026-01-11 04:45:04 -0300

    fix: fixed navbar icons to change color with hover/focus

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,13 +27,23 @@ export const Navbar = () => {
   const navItemClass = `
     flex
     flex-row
+    gap-2
     items-center
+    group/nav-item
     text-zinc-400
     hover:bg-transparent
     focus:bg-transparent
     focus-visible:bg-transparent
     hover:text-blue-400
     focus:text-blue-400
+  `;
+
+  const navIconClass = `
+    text-zinc-400
+    transition-colors
+    group-hover/nav-item:text-blue-400
+    group-focus/nav-item:text-blue-400
+    group-focus-visible/nav-item:text-blue-400
   `;
 
   return (
@@ -54,7 +64,7 @@ export const Navbar = () => {
             <NavigationMenuItem>
               <NavigationMenuLink asChild>
                 <Link to="/" className={navItemClass}>
-                  <Home size={18} />
+                  <Home size={18} className={navIconClass} />
                   <span>Home</span>
                 </Link>
               </NavigationMenuLink>
@@ -63,7 +73,7 @@ export const Navbar = () => {
             <NavigationMenuItem>
               <NavigationMenuLink asChild>
                 <Link to="/grade-curricular" className={navItemClass}>
-                  <Map size={18} />
+                  <Map size={18} className={navIconClass} />
                   <span>Grade Curricular</span>
                 </Link>
               </NavigationMenuLink>
@@ -72,7 +82,7 @@ export const Navbar = () => {
             <NavigationMenuItem>
               <NavigationMenuLink asChild>
                 <Link to="/meu-curso" className={navItemClass}>
-                  <BookMarked size={18} />
+                  <BookMarked size={18} className={navIconClass} />
                   <span>Meu Curso</span>
                 </Link>
               </NavigationMenuLink>
@@ -96,6 +106,7 @@ export const Navbar = () => {
                     strokeWidth="2"
                     strokeLinecap="round"
                     strokeLinejoin="round"
+                    className={navIconClass}
                   >
                     <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
                     <path d="M9 18c-4.51 2-5-2-7-2" />
