d71004bc4eb73e7e44f403467eaff9905894d7af
nayetdet <jpmoreiracampos21@gmail.com>
2026-02-06 09:19:00 -0300

    feat: implementada api

---
+++ src/app/api/cursos/[courseSlug]/etapas/[stepNumber]/disciplinas/[subjectNumber]/aulas/route.ts
@@ -0,0 +1,47 @@
+import { getCourse } from "@/services/course.service";
+import { getLessons } from "@/services/lesson.service";
+import Course from "@/types/course";
+import Lesson from "@/types/lesson";
+import Subject from "@/types/subject";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  _: Request,
+  { params }: { params: Promise<{ courseSlug: string; stepNumber: string; subjectNumber: string }> },
+) {
+  const { courseSlug, stepNumber, subjectNumber } = await params;
+  const parsedStepNumber: number = Number(stepNumber);
+  const parsedSubjectNumber: number = Number(subjectNumber);
+  if (!Number.isInteger(parsedStepNumber) || !Number.isInteger(parsedSubjectNumber)) {
+    return NextResponse.json(
+      { error: "Parâmetros inválidos." },
+      { status: 400 }
+    );
+  }
+
+  const course: Course | undefined = await getCourse(courseSlug);
+  if (!course) {
+    return NextResponse.json(
+      { error: "Curso não encontrado." },
+      { status: 404 }
+    );
+  }
+
+  const subject: Subject | undefined = course.steps?.[parsedStepNumber - 1]?.subjects?.[parsedSubjectNumber - 1];
+  if (!subject) {
+    return NextResponse.json(
+      { error: "Disciplina não encontrada." },
+      { status: 404 }
+    );
+  }
+
+  const lessons: Lesson[] | undefined = await getLessons(courseSlug, parsedStepNumber, subject.id);
+  if (!lessons) {
+    return NextResponse.json(
+      { error: "Aulas não encontradas." },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json(lessons);
+}
